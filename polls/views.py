1cd1ee312b8b52464d4385d23734024566b81d4f
smaheshacharya <mahesh01acharya@gmail.com>
2019-05-16 14:24:24 +0545

    add ajax method and convert get method into post

--- polls/views.py
+++ polls/views.py
@@ -5,18 +5,21 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 import requests
 from .train import *
+import codecs, json
+from numpyencoder import NumpyEncoder
+
 
 # import matplotlib.pyplot as plt
-
+with open('classify_data.pickle', 'rb') as pickle_saved_data:
+    unpickled_data = pickle.load(pickle_saved_data)
 def index(request):
     return render(request,'index.html')
-    
+
 def get_data(request ,*arg,**kwargs):
     naive_byes_test = GaussianNB()
     TestData = naive_byes_test.partial_fit(feature_array_test, labels_array_test, classes=np.unique(labels_array_test))
 
-    with open('classify_data.pickle', 'rb') as pickle_saved_data:
-        unpickled_data = pickle.load(pickle_saved_data)
+
         #predict data using pickle file
     predict_result = unpickled_data.predict(feature_array_test)
 
@@ -40,3 +43,35 @@ def get_data(request ,*arg,**kwargs):
 
     }
     return JsonResponse(data)
+
+
+def predict(request,*arg,**kargs):
+    if request.method == "GET":
+        search_word = request.GET["data_from_form"]
+        print(search_word)
+        each_input_word = []
+# change into array of word
+        each_input_word = search_word.split()
+
+#input data from user
+        length_input_data = len(each_input_word)
+
+        count_each_inputword = Counter(each_input_word)
+        input_data_tfvec = []
+# tf_each_input_word = []
+#TF computation of input data
+        for word,val in word_dict.items():#where word_dict is all the word collection from data set
+            if word in each_input_word:
+                count = count_each_inputword.get(word)
+                input_data_tfvec.append(count / float(length_input_data))
+            else:
+                input_data_tfvec.append(0)
+# to make predict input value similar as our training sample we use reshape
+        value_for_predict = np.array(input_data_tfvec).reshape(1,-1)
+        predict_data = unpickled_data.predict(value_for_predict)
+        int_data = int(np.asarray(predict_data))
+        result = {
+        "data":int_data,
+
+        }
+        return JsonResponse(result)
